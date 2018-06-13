18a476e4b197a30a6def25b2201f7612940e0289
DYbaobao <3295224486@qq.com>
2018-06-13 23:23:11 +0800

    因为项目启动时候报错--在行末尾处未插入";",但代码已经写了，经查找已将文件修改为utf-8编码

--- src/com/test/crm/dao/impl/ICommenDaoImpl.java
+++ src/com/test/crm/dao/impl/ICommenDaoImpl.java
@@ -25,79 +25,80 @@ import com.test.crm.util.GenericClass;
 
 
 
+
 @SuppressWarnings("unused")
 public class ICommenDaoImpl<T> extends HibernateDaoSupport implements ICommenDao<T> {
-    //获取父类的泛型类型
+	//获取父类的泛型类型
 	@SuppressWarnings("rawtypes")
 	private Class entityClass =GenericClass.getGenericClass(this.getClass());
-	
+
 	@Resource(name="sessionFactory")
-    public void setSessionFactionDI (SessionFactory sessionFactory){
-	   //调用父类的setSessionFactory方法,注入sessionFactory
-	 super.setSessionFactory(sessionFactory);
-    }
-   /**
-    *  <bean class="com.test.crm.dao.impl.ICommenDaoImpl">
-     <property name="sessionFactory" ref="sessionFactory"></property>
-      </bean>
-    */
+	public void setSessionFactionDI (SessionFactory sessionFactory){
+		//调用父类的setSessionFactory方法,注入sessionFactory
+		super.setSessionFactory(sessionFactory);
+	}
+	/**
+	 *  <bean class="com.test.crm.dao.impl.ICommenDaoImpl">
+	 <property name="sessionFactory" ref="sessionFactory"></property>
+	 </bean>
+	 */
 	@Override
 	public void save(T entity) {
-		this.getHibernateTemplate().save(entity);	
+		this.getHibernateTemplate().save(entity);
 	}
-   
-  
+
+
 	@Override
 	public void update(T entity) {
-        this.getHibernateTemplate().update(entity);
+		this.getHibernateTemplate().update(entity);
 	}
 	@SuppressWarnings("unchecked")
 	@Override
 	public T findObjectById(Serializable id) {
-	    if(id==null){
-	    	throw new RuntimeException("您要查找的["+id+"]不能为空");
-	    }
-	    /**
-	     *  getGenericSuperclass()  --泛型反射类 返回类型是type 
-	     *  ParameterizedType --参数化 的泛型类型
-	     *  getActualTypeArguments()---实际的类型
-	     */
+		if(id==null){
+			throw new RuntimeException("您要查找的["+id+"]不能为空");
+		}
+		/**
+		 *  getGenericSuperclass()  --泛型反射类 返回类型是type
+		 *  ParameterizedType --参数化 的泛型类型
+		 *  getActualTypeArguments()---实际的类型
+		 */
 	 /*   ParameterizedType type =
 	    		(ParameterizedType)this.getClass().getGenericSuperclass();
 	   @SuppressWarnings("rawtypes")
 	   Class entityClass = (Class) type.getActualTypeArguments()[0];*/
 		return (T)this.getHibernateTemplate().get(entityClass, id);
 	}
-	
-	
+
+
 	/**
 	 * 批量删除,通过多个id删除多个记录
 	 */
-	
+
 	@Override
 	public void deleteByIds(Serializable... ids) {
-		 if(ids!=null && ids.length>0){
-			 for(Serializable  id :ids){
-				 Object entity = this.getHibernateTemplate().get(entityClass, id);
-				 if(entity==null){
-					 throw new RuntimeException("您要查找的["+id+"]不存在");
-				 }
-				 this.getHibernateTemplate().delete(entity);
-			 }
-		 }
-		
+		if(ids!=null && ids.length>0){
+			for(Serializable  id :ids){
+				Object entity = this.getHibernateTemplate().get(entityClass, id);
+				if(entity==null){
+					throw new RuntimeException("您要查找的["+id+"]不存在");
+				}
+				this.getHibernateTemplate().delete(entity);
+			}
+		}
+
 	}
-	
-	
+
+
 	@Override
 	public void deleteAllObjects(Collection<T> entities) {
-	
+
 		this.getHibernateTemplate().deleteAll(entities);
 	}
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public List<T> findObjectsByConditionWithNoPage(String whereHql,
-		  final	Object [] params, LinkedHashMap<String, String> orderby) {
+													final	Object [] params, LinkedHashMap<String, String> orderby) {
 		//产生select 语句 select * from SysUserGroup o where
 		String hql = "select o from "+entityClass.getSimpleName() +" o where 1=1  ";
 		//在select 语句后面组合select 语句的查询条件
@@ -109,41 +110,41 @@ public class ICommenDaoImpl<T> extends HibernateDaoSupport implements ICommenDao
 		hql = hql+orderbyStr;
 		System.out.println("hql  "+ hql);
 		final String fhql =hql;
-	    List<T> list =(List <T>) this.getHibernateTemplate().execute(new HibernateCallback() {
-			
+		List<T> list =(List <T>) this.getHibernateTemplate().execute(new HibernateCallback() {
+
 			@Override
 			public Object doInHibernate(Session session) throws HibernateException,SQLException {
-			    Query query =  session.createQuery(fhql); 
-			    setParams(query ,params);
+				Query query =  session.createQuery(fhql);
+				setParams(query ,params);
 				return query.list();
 			}
 
 		});
 		return list;
 	}
-    /*
+	/*
      * 设置hql 语句所需的参数
      */
 	public void setParams(Query query, Object[] params) {
 		if(params != null && params.length>0){
-	    	for (int i = 0; i < params.length; i++) {
-				 query.setParameter(i, params[i]);
+			for (int i = 0; i < params.length; i++) {
+				query.setParameter(i, params[i]);
 			}
-	    }
-		
+		}
+
 	}
 	public  String buildOrderby(LinkedHashMap<String, String> orderby) {
-	   StringBuffer sbf = new StringBuffer(" ");
-	   if(orderby!=null && !orderby.isEmpty()){
-		   sbf.append(" order by  ");
-		   for (Map.Entry<String, String>  em : orderby.entrySet()) {
-			   sbf.append(em.getKey()+"  "+em.getValue()+",");
-			   
+		StringBuffer sbf = new StringBuffer(" ");
+		if(orderby!=null && !orderby.isEmpty()){
+			sbf.append(" order by  ");
+			for (Map.Entry<String, String>  em : orderby.entrySet()) {
+				sbf.append(em.getKey()+"  "+em.getValue()+",");
+
+			}
+			//去掉最后一个","
+			sbf.deleteCharAt(sbf.length()-1);
 		}
-		   //去掉最后一个","
-		   sbf.deleteCharAt(sbf.length()-1);
-	   }
 		return sbf.toString();
 	}
-	 
+
 }
